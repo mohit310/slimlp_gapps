ac7e8c00900b326ad08759ae9802089773c3b4ea
Mohit Keswani <mohit.keswani@gmail.com>
2015-09-30 12:40:22 -0400

    modified dynamic.sh

--- structure/dynamic.sh
+++ structure/dynamic.sh
@@ -10,5 +10,8 @@ cp -af /tmp/Swypelib/lib/* /system/lib
 
 # FaceLock #zero
 cp -af /tmp/FaceLock/arm/* /system #zero
+cp -af /tmp/FaceLock/vendor/* /system/vendor #zero
+
 # Velvet #zero
+cp -af /tmp/Velvet/usr/* /system/usr #zero
 cp -af /tmp/Velvet/arm/* /system #zero
