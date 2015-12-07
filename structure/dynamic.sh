ad362cf96832034c10d256f7451eac787566af14
Mohit Keswani <mohit.keswani@gmail.com>
2015-12-06 22:34:21 -0500

    removed 64 bit as this is for one plus one

--- structure/dynamic.sh
+++ structure/dynamic.sh
@@ -1,48 +1,18 @@
 #!/sbin/sh
 
-# Variables
-ARCH=`grep ro.product.cpu.abi= /system/build.prop | cut -d "=" -f 2`
-SETUPWIZARD=`grep ro.build.characteristics /system/build.prop | grep tablet`
-
 # PrebuiltGmsCore
-if [ $ARCH == armeabi-v7a ]; then
- cp -rf /tmp/PrebuiltGmsCore/arm/* /system
-elif [ $ARCH == arm64-v8a ]; then
- cp -rf /tmp/PrebuiltGmsCore/arm64/* /system
-fi
+cp -rf /tmp/PrebuiltGmsCore/arm/* /system
 
 # SetupWizard
-if [ ! $SETUPWIZARD == "" ]; then
- cp -rf /tmp/SetupWizard/tablet/* /system
-else
- cp -rf /tmp/SetupWizard/phone/* /system
-fi
+cp -rf /tmp/SetupWizard/phone/* /system
 
 # LatinIME swypelib
-if [ $ARCH == armeabi-v7a ]; then
- cp -rf /tmp/LatinIME_swypelib/lib/* /system/lib
-elif [ $ARCH == arm64-v8a ]; then
- cp -rf /tmp/LatinIME_swypelib/lib64/* /system/lib64
-else
- cp -rf /tmp/LatinIME_swypelib/lib/* /system/lib
-fi
+cp -rf /tmp/LatinIME_swypelib/lib/* /system/lib
 
 # FaceLock #mini
-if [ $ARCH == armeabi-v7a ]; then #mini
- cp -rf /tmp/FaceLock/arm/* /system #mini
-elif [ $ARCH == arm64-v8a ]; then #mini
- cp -rf /tmp/FaceLock/arm64/* /system #mini
-else #mini
- cp -rf /tmp/FaceLock/arm/* /system #mini
-fi #mini
+cp -rf /tmp/FaceLock/arm/* /system #mini
 
 # Velvet #mini
-if [ $ARCH == armeabi-v7a ]; then #mini
- cp -rf /tmp/Velvet/arm/* /system #mini
-elif [ $ARCH == arm64-v8a ]; then #mini
- cp -rf /tmp/Velvet/arm64/* /system #mini
-else #mini
- cp -rf /tmp/Velvet/arm/* /system #mini
-fi #mini
+cp -rf /tmp/Velvet/arm/* /system #mini
 
 exit 0
