800389fb74a504a6f894f462f575cc2d80000cda
Mohit Keswani <mohit.keswani@gmail.com>
2015-12-08 20:07:08 -0500

    modified dynamic

--- structure/dynamic.sh
+++ structure/dynamic.sh
@@ -8,9 +8,14 @@ cp -rf /tmp/SetupWizard/phone/* /system
 
 # LatinIME swypelib
 cp -rf /tmp/LatinIME_swypelib/lib/* /system/lib
+mkdir -p /system/app/LatinIME/lib/arm
+ln -s /system/lib/libjni_latinime.so /system/app/LatinIME/lib/arm/libjni_latinime.so
+ln -s /system/lib/libjni_latinimegoogle.so /system/app/LatinIME/lib/arm/libjni_latinimegoogle.so
 
 # FaceLock #zero
 cp -rf /tmp/FaceLock/arm/* /system #zero
+mkdir -p /system/app/FaceLock/lib/arm #zero
+ln -s /system/lib/libfacelock_jni.so /system/app/FaceLock/lib/arm/libfacelock_jni.so #zero
 
 # Velvet #zero
 cp -rf /tmp/Velvet/arm/* /system #zero
