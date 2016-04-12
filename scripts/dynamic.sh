9f27e1701e10fb5601724a1e350bdd8e133575be
Mohit Keswani <mohit.keswani@gmail.com>
2016-04-12 01:23:58 -0400

    Merge remote-tracking branch 'remotes/upstream/mm6.0' into mm6.0
    # Conflicts:
    #	build.xml
    #	scripts/dynamic.sh
    #	structure/dynamic/FaceLock/arm64/app/FaceLock/FaceLock.apk
    #	structure/dynamic/FaceLock/arm64/lib64/libfacelock_jni.so
    #	structure/dynamic/FaceLock/arm64/lib64/libfilterpack_facedetect.so
    #	structure/dynamic/FaceLock/arm64/vendor/lib/libfrsdk.so
    #	structure/dynamic/FaceLock/arm64/vendor/lib64/libfrsdk.so
    #	structure/dynamic/LatinIME_swypelib/lib64/libjni_latinime.so
    #	structure/dynamic/LatinIME_swypelib/lib64/libjni_latinimegoogle.so
    #	structure/dynamic/SetupWizard/tablet/priv-app/SetupWizard/SetupWizard.apk
    #	structure/dynamic/Velvet/arm64/priv-app/Velvet/Velvet.apk

--- scripts/dynamic.sh
+++ scripts/dynamic.sh
@@ -9,16 +9,15 @@ cp -rf /tmp/SetupWizard/phone/* /system
 # LatinIME swypelib
 cp -rf /tmp/LatinIME_swypelib/lib/* /system/lib
 mkdir -p /system/app/LatinIME/lib/arm
-ln -s /system/lib/libjni_latinime.so /system/app/LatinIME/lib/arm/libjni_latinime.so
-ln -s /system/lib/libjni_latinimegoogle.so /system/app/LatinIME/lib/arm/libjni_latinimegoogle.so
+ln -sfn /system/lib/libjni_latinime.so /system/app/LatinIME/lib/arm/libjni_latinime.so
+ln -sfn /system/lib/libjni_latinimegoogle.so /system/app/LatinIME/lib/arm/libjni_latinimegoogle.so
+ln -sfn /system/lib/libjni_keyboarddecoder.so /system/app/LatinIME/lib/arm/libjni_keyboarddecoder.so
 
-# libfrsdk
-cp -rf /tmp/libfrsdk/arm/* /system
 
 # FaceLock #zero
 cp -rf /tmp/FaceLock/arm/* /system #zero
 mkdir -p /system/app/FaceLock/lib/arm #zero
-ln -s /system/lib/libfacelock_jni.so /system/app/FaceLock/lib/arm/libfacelock_jni.so #zero
+ln -sfn /system/lib/libfacelock_jni.so /system/app/FaceLock/lib/arm/libfacelock_jni.so #zero
 
 # Velvet #zero
 cp -rf /tmp/Velvet/arm/* /system #zero
